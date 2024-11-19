2d327581aa538ffd09f612eb53638ddac5e19b46
Andrey Dyupin <adiupin@astralinux.ru>
2024-11-19 18:37:27 +0300

    Listeners reformating for class-manager

--- trpo_site/products/urls.py
+++ trpo_site/products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import product_list, product_add, product_edit, product_delete
+from .views import ProductWorker
 
 urlpatterns = [
-    path('', product_list, name='product_list'),
-    path('add/', product_add, name='product_add'),
-    path('edit/<int:pk>/', product_edit, name='product_edit'),
-    path('delete/<int:pk>/', product_delete, name='product_delete'),
+    path('', ProductWorker.product_list, name='product_list'),
+    path('add/', ProductWorker.product_add, name='product_add'),
+    path('edit/<int:pk>/', ProductWorker.product_edit, name='product_edit'),
+    path('delete/<int:pk>/', ProductWorker.product_delete, name='product_delete'),
 ]
--- trpo_site/products/views.py
+++ trpo_site/products/views.py
@@ -7,37 +7,39 @@ from .models import Product
 from .forms import ProductForm
 
 
-def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
+class ProductWorker:
+    @classmethod
+    def product_list(cls, request):
+        products = Product.objects.all()
+        return render(request, 'products/product_list.html', {'products': products})
 
+    @classmethod
+    def product_add(cls, request):
+        if request.method == 'POST':
+            form = ProductForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('product_list')
+        else:
+            form = ProductForm()
+        return render(request, 'products/product_form.html', {'form': form})
 
-def product_add(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            form.save()
+    @classmethod
+    def product_edit(cls, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        if request.method == 'POST':
+            form = ProductForm(request.POST, instance=product)
+            if form.is_valid():
+                form.save()
+                return redirect('product_list')
+        else:
+            form = ProductForm(instance=product)
+        return render(request, 'products/product_form.html', {'form': form})
+
+    @classmethod
+    def product_delete(cls, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        if request.method == 'POST':
+            product.delete()
             return redirect('product_list')
-    else:
-        form = ProductForm()
-    return render(request, 'products/product_form.html', {'form': form})
-
-
-def product_edit(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'POST':
-        form = ProductForm(request.POST, instance=product)
-        if form.is_valid():
-            form.save()
-            return redirect('product_list')
-    else:
-        form = ProductForm(instance=product)
-    return render(request, 'products/product_form.html', {'form': form})
-
-
-def product_delete(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'POST':
-        product.delete()
-        return redirect('product_list')
-    return render(request, 'products/product_confirm_delete.html', {'product': product})
+        return render(request, 'products/product_confirm_delete.html', {'product': product})
