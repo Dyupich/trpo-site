08540e82137b111f055d9e9f9cae1bc5ddaff436
Andrey Dyupin <adiupin@astralinux.ru>
2024-11-23 20:20:39 +0300

    Added new fields for Product with validation

--- trpo_site/products/models.py
+++ trpo_site/products/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=30)
+    category = models.CharField(max_length=30, default="Без категории")
+    quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    incoming_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
