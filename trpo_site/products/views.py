ff91a5f7f351835d0e7d48b57485890e40c2c6a2
Andrey Dyupin <adiupin@astralinux.ru>
2024-11-23 20:40:42 +0300

    Added filtration

--- trpo_site/products/views.py
+++ trpo_site/products/views.py
@@ -11,6 +11,31 @@ class ProductWorker:
     @classmethod
     def product_list(cls, request):
         products = Product.objects.all()
+
+        # Обработка фильтрации
+        name = request.GET.get('name')
+        category = request.GET.get('category')
+        min_price = request.GET.get('min_price')
+        max_price = request.GET.get('max_price')
+        min_quantity = request.GET.get('min_quantity')
+        max_quantity = request.GET.get('max_quantity')
+        incoming_date = request.GET.get('incoming_date')
+
+        if name:
+            products = products.filter(name__icontains=name)
+        if category:
+            products = products.filter(category__icontains=category)
+        if min_price:
+            products = products.filter(price__gte=min_price)
+        if max_price:
+            products = products.filter(price__lte=max_price)
+        if min_quantity:
+            products = products.filter(quantity__gte=min_quantity)
+        if max_quantity:
+            products = products.filter(quantity__lte=max_quantity)
+        if incoming_date:
+            products = products.filter(incoming_date__date=incoming_date)
+
         return render(request, 'products/product_list.html', {'products': products})
 
     @classmethod
