89cd27923b4ba7fc878572a9712fce4388272279
Natalia Ligonen <natalia1326@yandex.ru>
2024-12-10 18:47:28 +0300

    Front-end Modification

--- trpo_site/trpo_site/urls.py
+++ trpo_site/trpo_site/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #path('admin/', admin.site.urls),
+    #path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
+    path('', include('products.urls')),  # Подключаем маршруты приложения products к корню
+
 ]
